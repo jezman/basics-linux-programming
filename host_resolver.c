772926cf02333982a1c1f38f6e440fd318ba78ea
jezman <dev.jezman@gmail.com>
2018-12-06 11:24:31 +0300

    Lan.

---
+++ host_resolver.c
@@ -0,0 +1,32 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+        return -1;
+
+    struct hostent *h;
+
+    h = gethostbyname(argv[1]);
+
+    if (h == NULL)
+    {
+        printf("error\n");
+        return -1;
+    }
+
+    int i = 0;
+
+    while (h->h_addr_list[i] != NULL)
+    {
+        struct in_addr *a = (struct in_addr *)h->h_addr_list[i];
+        printf("%s\n", inet_ntoa(*a));
+        i++;
+    }
+
+    return 0;
+}
